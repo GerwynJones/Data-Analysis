97ac998fc6c631c564a9bb2e141206acd0be2eda
Gerwyn Jones <jonesg71@cardiff.ac.uk>
2017-11-04 17:57:08 +0000

    Assignment 2 update

--- Assignments/Assignment 2.py
+++ Assignments/Assignment 2.py
@@ -10,5 +10,92 @@ import numpy as np
 import scipy.stats as sc
 import matplotlib.pyplot as plt
 
+plt.close('all')
 
-print "Hello SSH Test"
+
+#########################################################
+
+# Question 1
+
+def plikelihood(v, N):
+
+    theta = np.linspace(0, 1, 100)
+
+    p = (theta**v)*((1-theta)**(N-v))
+
+    return p/np.sum(p)
+
+
+def pprior(a, b):
+
+    theta = np.linspace(0, 1, 100)
+
+    p = (theta**(a-1))*((1-theta)**(b-1))
+
+    return p
+
+def pposterior(v, N, a, b):
+
+    theta = np.linspace(0, 1, 100)
+
+    post = (theta**((v + a)-1))*((1-theta)**((N - v + b)-1))
+
+    norm_post = post/np.sum(post)
+
+    return norm_post
+
+v=0; N=3; a=1; b=1
+
+theta = np.linspace(0, 1, 100)
+likelihood = plikelihood(v, N)
+prior = pprior(a, b)
+posterior = pposterior(v, N, a, b)
+
+print np.mean(likelihood)
+print np.std(likelihood)
+
+print np.sum(posterior)
+
+plt.figure()
+
+plt.plot(theta, likelihood)
+
+plt.figure()
+
+plt.plot(theta, prior)
+
+plt.figure()
+
+plt.plot(theta, posterior)
+
+#########################################################
+
+# Question 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+plt.show()
