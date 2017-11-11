2cd4927ec5c820fb00e8af455fb376f9d0623910
Gerwyn Jones <jonesg71@cardiff.ac.uk>
2017-11-11 12:40:31 +0000

    Assignment 3 Start
    Signed-off-by: Gezerj <JonesG71@cardiff.ac.uk>

---
+++ Assignments/Assignment 3/Assignment 3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 04 10:57:34 2017
+
+@author: Gerwyn
+"""
+
+from __future__ import division
+import numpy as np
+import scipy.stats as sc
+import matplotlib.pyplot as plt
+
+
+
+
